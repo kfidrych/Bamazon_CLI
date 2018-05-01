c2349450e3bfad71267dd72b19eee778c60820d5
Kristian Fidrych <kristianfidrych@gmail.com>
2018-05-01 15:36:33 -0400

    added Add new item and Delete item functions. Working. All Manager functions complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,7 +20,7 @@ function start() {
         type: "list",
         name: "function",
         message: "Please select a function to perform: ",
-        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Delete an Item"]
     }).then(function (response) {
         switch (response.function) {
             case "View Products for Sale":
@@ -34,6 +34,9 @@ function start() {
                 break;
             case "Add New Product":
                 addNewProduct();
+                break;
+            case "Delete an Item":
+                deleteItem();
                 break;
         };
     });
@@ -89,7 +92,7 @@ function addInventory() {
             message: "How much stock would you like to add?"
         }
     ]).then(function (response) {
-        connection.query("SELECT * FROM products WHERE ?", {item_id: response.itemChoice}, function (err, results) {
+        connection.query("SELECT * FROM products WHERE ?", { item_id: response.itemChoice }, function (err, results) {
             if (err) throw err;
             var itemStock = parseInt(results[0].stock_quantity);
             var addQuantity = parseInt(response.addQuantity);
@@ -104,6 +107,54 @@ function addInventory() {
                 };
             console.log("Inventory Updated!")
             newFunction();
-        })
-    })
-}
+        });
+    });
+};
+
+function addNewProduct() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemName",
+            message: "What is the name of the new item?"
+        },
+        {
+            type: "input",
+            name: "itemCategory",
+            message: "What is the category of the new item?"
+        },
+        {
+            type: "input",
+            name: "itemPrice",
+            message: "What is the price of the new item?"
+        },
+        {
+            type: "input",
+            name: "itemStock",
+            message: "What is the starting inventory of the new item?"
+        }
+    ]).then(function (response) {
+        var newItemInfo = { product_name: response.itemName, department_name: response.itemCategory, price: response.itemPrice, stock_quantity: response.itemStock };
+        connection.query("INSERT INTO products SET ?", newItemInfo, function (err, results) {
+            if (err) throw err;
+        });
+        console.log("New Item Added to Inventory!");
+        newFunction();
+    });
+};
+
+function deleteItem() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemChoice",
+            message: "Please enter the item ID to delete: "
+        }
+    ]).then(function (response) {
+        connection.query("DELETE FROM products WHERE ?", { item_id: response.itemChoice }, function (err, results) {
+            if (err) throw err;
+        });
+        console.log("Item Deleted from Inventory!")
+        newFunction();
+    });
+};
